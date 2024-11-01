064c7dc646303066109329fe480c8780df4fc849
Abhinav.K <134003682+Abhinavkunnummal@users.noreply.github.com>
2024-11-01 13:38:26 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)  //ReferenceError: alert is not defined We are using node js not browser
+
+let name="Abhi"
+let age=15
+let isLoggedIn=true
+
+
+// number => 2 to the power 53
+// bigInt 
+// string => ""
+// boolean =>true/false
+// null =>standalone value
+// undefined => not assigning any value 
+// symbol => unique
+
+
+// object
+console.log(typeof null); // null is an object
+console.log(typeof undefined); // undefined
+
